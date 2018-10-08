3cb53775ebf1e470ffd8a0f22e61e17742060eaf
Bastien <bastien.guillaumat@epitech.eu>
2018-10-08 19:51:14 +0200

    [SERVER] Bin Protocol

--- sources/common/Binary.cpp
+++ sources/common/Binary.cpp
@@ -11,11 +11,7 @@
 std::string	strToBinary(std::string	s){
 	std::string	value;
 	std::string	retVal;
-<<<<<<< HEAD
-	int n = s.length()-1;
-=======
-	int n = (int)s.length();
->>>>>>> c043ef0cf22bd343e244002a2f80f7d54d122ece
+	int n = (int)s.length()-1;
 
 	for (int i = 0; i <= n; i++)
 	{
