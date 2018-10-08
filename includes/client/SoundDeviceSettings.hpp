6f4da9a6a1bbcbe218a4e5918345c91dd3a917f3
AsianPw <brice.lang-nguyen@epitech.eu>
2018-10-08 15:09:45 +0200

    [Sound] framePerBuffer

--- includes/client/SoundDeviceSettings.hpp
+++ includes/client/SoundDeviceSettings.hpp
@@ -3,7 +3,7 @@ namespace Babel {
 	namespace SoundDeviceSetting {
 		static constexpr unsigned long sampleRate = 48000;
 		static constexpr unsigned long channels   = 2;
-		static constexpr unsigned long framePerBuffer = 5;
+		static constexpr unsigned long framePerBuffer = 480;
 		static constexpr float         SAMPLE_SILENCE = 0.0f;
 	}
 }
